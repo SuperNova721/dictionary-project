72776011b0fe43630ffac1597aa4d0fc5023ab72
SuperNova721 <94159279+SuperNova721@users.noreply.github.com>
2022-04-14 17:04:16 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="dictionary-header">
+        Dictionary
+      </header>
+    </div>
+  );
+}
+
+export default App;
