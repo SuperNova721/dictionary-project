5ab8df4e13fc53a12e31a12361c69ce2bb4fe607
SuperNova721 <94159279+SuperNova721@users.noreply.github.com>
2022-04-16 18:25:38 -0700

    Added css to style app appearance and create responsive links and buttons.

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -12,7 +12,7 @@ export default function Phonetics(props) {
     } else {
         return (
             <div className="Phonetics">
-                <a href={props.phonetics.audio} target="_blank" rel="noreferrer">Listen</a>    
+                <a className="audio-link" href={props.phonetics.audio} target="_blank" rel="noreferrer">Listen</a>    
                 <br />
                 {props.phonetics.text}
             </div>
